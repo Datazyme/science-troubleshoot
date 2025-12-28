690de60b972a9408d6285466813e8f9756921689
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mac-mini.local>
2025-12-27 17:58:54 -0800

    with connection to new mongodb cluster scienceTroubleshootCluster

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(express.json({extended: true}))
 app.use(cors({credentials: true, origin: ["http://localhost:5173"]})) //replaced with front end url
 app.use(upload())
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
+connect(process.env.MONGO_URL).then(app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))).catch(err => console.log(err))
+//npm run dev
 
 
