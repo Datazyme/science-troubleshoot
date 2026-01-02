2488416b341e25167d0b3a960305e8100c0ffd83
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mac-mini.local>
2026-01-01 18:35:22 -0800

    with saved routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,14 @@
 const router = require("express").Router()
 
 const {registerUser, loginUser, getUser, getUsers, editUser, followUnfollowUser, changeUserAvatar} = require('../controllers/userControllers')
+const authMiddleware = require("../middleware/authMiddleware")
 
 //User Routes
 router.post('/users/register', registerUser)
 router.post('/users/login', loginUser)
 router.get('/users/:id', getUser)
 router.get('/users', getUsers)
-router.patch('/users/:id', editUser)
+router.patch('/users/:id', authMiddleware, editUser)
 router.get('/users/:id/follow-unfollow', followUnfollowUser)
 router.post('/users/avatar', changeUserAvatar)
 
