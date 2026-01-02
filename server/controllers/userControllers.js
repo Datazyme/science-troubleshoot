d2e9950f4a32af3e840175c3a1afdb40a0bb9a10
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mac-mini.local>
2026-01-01 18:28:42 -0800

    with all routes, authmiddleware, usercontrollers with register, login, get user and users

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,8 @@
 const HttpError = require('../models/errorModel')
+const userModel = require('../models/userModel')
+const bcrypt = require("bcryptjs")
+const jtoken = require("jsonwebtoken")
+
 
 //===========Register User
 //POST : api/users/register
@@ -6,7 +10,32 @@ const HttpError = require('../models/errorModel')
 
 const registerUser = async (req, res, next) => {
     try {
-        res.json("Register User")
+        const {fullName, email, password, confirmPassword} = req.body
+        if(!fullName || !email || !password || !confirmPassword) {
+            return next(new HttpError("Fill in all fields", 422)) //422 is invalid input from user
+        }
+        //make the email lowercase
+        const lowerCaseEmail = email.toLowerCase();
+        //check db if email exists
+        const emailExists = await userModel.findOne({email: lowerCaseEmail})
+        if(emailExists) {
+            return next(new HttpError("Error already exists", 422))
+        }
+        //check if password and confirm password match
+        if(password !== confirmPassword) {
+            return next(new HttpError("Passwords do not match"))
+        }
+        //check password length
+        if (password.length < 6) {
+            return next(new HttpError("Password should be more than 6 characters", 422))
+        }
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        //add user to db
+        const newUser = await userModel.create({fullName, email: lowerCaseEmail, password: hashedPassword})
+        res.json(newUser).status(201);
+
     } catch (error) {
         return next(new HttpError(error))
     }
@@ -16,9 +45,33 @@ const registerUser = async (req, res, next) => {
 //POST : api/users/login
 //Unprotected
 
-const loginrUser = async (req, res, next) => {
+const loginUser = async (req, res, next) => {
     try {
-        res.json("Login User")
+        const {email, password} = req.body
+        if(!email || !password) {
+            return next(new HttpError("Fill in all fields", 422)) //422 is invalid input from user
+        }
+
+        //make the email lowercase
+        const lowerCaseEmail = email.toLowerCase();
+
+        //fetch user from db
+        const yuser = await userModel.findOne({email: lowerCaseEmail})
+        if(!yuser) {
+            return next(new HttpError("Invalid credentials", 422))
+        }
+        const {uPassword, ...userInfo} = yuser;
+
+        //compare passwords
+        const comparedPass = await bcrypt.compare(password, yuser?.password);
+        if(!comparedPass) {
+            return next(new HttpError("Invalid credentials", 422)) 
+        }
+        const userToken = await jtoken.sign({id: yuser?._id}, process.env.JWT_SECRET,
+        {expiresIn: "1hr"})
+        res.json({userToken, id: yuser?._id}).status(200);
+        //res.json({userToken, id: yuser?._id, ...userInfo}).status(200);
+       
     } catch (error) {
         return next(new HttpError(error))
     }
@@ -30,7 +83,13 @@ const loginrUser = async (req, res, next) => {
 
 const getUser = async (req, res, next) => {
     try {
-        res.json("Get User")
+        const {id} = req.params;
+        const user = await userModel.findById(id)
+        if(!user) {
+            return next(new HttpError("Could not find registered user", 422))
+        }
+        res.json(user).status(200)
+        //res.json("Get User")
     } catch (error) {
         return next(new HttpError(error))
     }
@@ -42,7 +101,8 @@ const getUser = async (req, res, next) => {
 
 const getUsers = async (req, res, next) => {
     try {
-        res.json("Get Users")
+        const users = await userModel.find().limit(10).sort({createdAt: -1})
+        res.json(users)
     } catch (error) {
         return next(new HttpError(error))
     }
@@ -54,17 +114,20 @@ const getUsers = async (req, res, next) => {
 
 const editUser = async (req, res, next) => {
     try {
-        res.json("Edit User")
+        const {fullName, bio} = req.body
+        const editedUser = await userModel.findByIdAndUpdate(req.user.id,
+        {fullName, bio}, {new: true})
+        res.json(editUser).status(200)
     } catch (error) {
         return next(new HttpError(error))
     }
 }
 
 //================Follow/Unfollow User
-//PATCH : api/user/edit
+//Get : api/user/edit
 //Protected
 
-const followUnfollowUser= async (req, res, next) => {
+const followUnfollowUser = async (req, res, next) => {
     try {
         res.json("Follow/Unfollow User")
     } catch (error) {
@@ -74,7 +137,7 @@ const followUnfollowUser= async (req, res, next) => {
 
 
 //================Change User Profile Pic
-//PATCH : api/user/avatar
+//Post : api/user/avatar
 //Protected
 
 const changeUserAvatar = async (req, res, next) => {
@@ -85,4 +148,4 @@ const changeUserAvatar = async (req, res, next) => {
     }
 }
 
-module.exports = {registerUser, loginrUser, getUser, getUsers, editUser, followUnfollowUser, changeUserAvatar}
+module.exports = {registerUser, loginUser, getUser, getUsers, editUser, followUnfollowUser, changeUserAvatar}
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ require("dotenv").config()
 const cors = require("cors")
 const upload = require("express-fileupload")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware")
+const routes = require('./routes/routes')
 
 const app = express()
 
@@ -19,8 +20,12 @@ app.use(express.json({extended: true}))
 app.use(cors({credentials: true, origin: ["http://localhost:5173"]})) //replaced with front end url
 app.use(upload())
 
+app.use('/api', routes)
+
 app.use(notFound);
 app.use(errorHandler);
+
+
 
 connect(process.env.MONGO_URL).then(app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))).catch(err => console.log(err))
 //npm run dev
